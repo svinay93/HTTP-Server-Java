4b4b826ad8aba03c00ac291948c50e4a59dc66b8
Ashwini Sunil <ashwinisunil@ashwinis-mbp.mynetworksettings.com>
2024-08-24 18:25:43 -0400

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,20 +17,35 @@ public class Main {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            while(true) {
-                Socket client = serverSocket.accept(); // Wait for connection from client.
-                byte[] in = client.getInputStream().readNBytes(70);
-                String str = new String(in, StandardCharsets.UTF_8);
-                String data = str.split("\r\n")[0].split(" ")[1];
-                System.out.println(data);
-                if (data.equals("/") && data.length() == 1) {
-                    client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                } else {
-                    client.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-                }
-                client.close();
-                System.out.println("accepted new connection");
+
+            Socket client = serverSocket.accept(); // Wait for connection from client.
+
+            InputStream input = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String line = reader.readLine();
+            System.out.println(line);
+            String[] HttpRequest = line.split(" ", 0);
+            OutputStream output = client.getOutputStream();
+            output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            if (HttpRequest[1].equals("/")) {
+                output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else {
+                output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
             }
+
+
+//            byte[] in = client.getInputStream().readNBytes(70);
+//            String str = new String(in, StandardCharsets.UTF_8);
+//            String data = str.split("\r\n")[0].split(" ")[1];
+//            System.out.println(data);
+//            if (data.equals("/") && data.length() == 1) {
+//                client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+//            } else {
+//                client.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+//            }
+
+            System.out.println("accepted new connection");
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
