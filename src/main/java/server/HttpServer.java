ffa5f54a27d193caca1d96cb8a1d17ac74190397
Ashwini Sunil <ashwinisunil@ashwinis-mbp.mynetworksettings.com>
2024-08-29 22:55:40 -0400

    pass stage

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -5,6 +5,7 @@ package server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.zip.GZIPOutputStream;
 
 
 public class HttpServer {
@@ -23,7 +25,7 @@ public class HttpServer {
     private static final String CREATED_201 = "HTTP/1.1 201 Created\r\n\r\n";
     private static final String PLAIN_TEXT_200 = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
     private static final String OCTET_STREAM_200 = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ";
-    private static final String GZIP_BASE = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\n\r\n";
+    private static final String GZIP_BASE = "HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: ";
 
 
 
@@ -105,7 +107,17 @@ public class HttpServer {
         else if (requestPath.startsWith("/echo")) {
             String data = requestPath.split("/")[2];
             if(requestHeaders.containsKey("Accept-Encoding") && requestHeaders.get("Accept-Encoding").contains("gzip")){
-                client.getOutputStream().write(GZIP_BASE.getBytes());
+                byte[] compressedData = compress(data);
+                byte[] base = (GZIP_BASE + compressedData.length + "\r\n\r\n").getBytes();
+                byte[] allByteArray = new byte[base.length + compressedData.length ];
+                ByteBuffer buff = ByteBuffer.wrap(allByteArray);
+                buff.put(base);
+                buff.put(compressedData);
+                byte[] combined = buff.array();
+                client.getOutputStream().write(base);
+                client.getOutputStream().write(compressedData);
+                
+
             }
             else {
                 sendResponse(PLAIN_TEXT_200, data, client);
@@ -135,5 +147,18 @@ public class HttpServer {
         String res = base + data.length() + "\r\n\r\n" + data;
         client.getOutputStream().write(res.getBytes());
     }
+
+    private byte[] compress(final String str) throws IOException {
+        if ((str == null) || (str.length() == 0)) {
+            return null;
+        }
+        ByteArrayOutputStream obj = new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(obj);
+        gzip.write(str.getBytes("UTF-8"));
+        gzip.flush();
+        gzip.close();
+        return obj.toByteArray();
+    }
+
 
 }
